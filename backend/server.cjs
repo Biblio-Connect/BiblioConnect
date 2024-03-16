2d1af194d130a109c9aa3cb3cd93e16a42c766f8
Eric Huang <ejwhuang@gmail.com>
2024-03-16 16:42:34 -0700

    added database and query for first 12 books in backend

---
+++ backend/server.cjs
@@ -0,0 +1,37 @@
+/* eslint-disable no-undef */
+const express = require("express");
+const path = require("path");
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database("book.sqlite3");
+
+const app = express();
+const PORT = process.env.PORT || 3333;
+
+// Serve Vite output as static files
+app.use(express.static(path.join(__dirname, "dist")));
+
+// get all the items
+app.get("/api/items", (req, res) => {
+  console.log("API Request:", req.url);
+  const statement = `
+  SELECT
+    ImageURL, 
+    Name, 
+    Author,
+    Genres,
+    Chapters, 
+    Description
+  FROM Books LIMIT 12;`;
+  db.all(statement, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json(rows);
+  });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
+
