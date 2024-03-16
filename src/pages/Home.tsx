16fcd8557c599b3e5965e27840e9788fb2473984
Sean Finch <finch.s@northeastern.edu>
2024-03-16 01:45:00 -0700

    Remove duplicate title

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,15 +12,14 @@ function Home() {
       className={`flex justify-center ${theme === "light" ? "bg-light-mode text-ultra-dark-mode" : "bg-dark-mode text-light-mode"}`}
     >
       <div className="w-full">
-        <div className="w-full h-full flex flex-col justify-center items-center">
+        <div className="mb-4 w-full h-full flex flex-col justify-center items-center">
           <img
             src="/BiblioConnectFull.svg"
             className="object-cover h-full rounded-lg"
-            style={{ maxHeight: "200px", width: "auto" }}
+            style={{ maxHeight: "300px", width: "auto" }}
           />
         </div>
         <div className="flex flex-col items-center text-center">
-          <p className="text-7xl font-extrabold">BiblioConnect</p>
           <p className="mb-4 mx-4 font-bold">
             Find your next favorite book with just a few swipes
           </p>
